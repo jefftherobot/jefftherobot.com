7acad200b96431db0f739b2a711d6d31ce3bd373
Jeff Mills <jeffy9@gmail.com>
2015-08-07 21:07:01 -0400

    add run to help command

--- src/js/commands/help.js
+++ src/js/commands/help.js
@@ -8,6 +8,7 @@ var message = [
 	'    [[;#A1B56C;]help] Shows this menu',
 	'    [[;#A1B56C;]greeting] Shows the greeting message',
 	'    [[;#A1B56C;]motd] Shows a programmer quote from https://github.com/AntJanus/programmers-proverbs',
+	'    [[;#A1B56C;]run] Takes one parameter',
 	'    [[;#A1B56C;]fullscreen]',
 	'        [[;#F7CA88;]--enable] Go fullscreen ',
 	'        [[;#F7CA88;]--disable] Exit fullscreen ',
