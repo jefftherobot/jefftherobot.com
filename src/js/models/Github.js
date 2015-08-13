3501edc91a76b33821ffe34c0a308b11203bb897
Jeff <jeffy9@gmail.com>
2015-08-13 18:03:57 -0400

    Start github integration

---
+++ src/js/models/Github.js
@@ -0,0 +1,15 @@
+
+class Github {
+	constructor (opts) {
+		const API_URL = 'https://api.github.com';
+		const OWNER = 'jefftherobot';
+		const REPO = 'jefftherobot.com'
+
+
+		 $.getJSON(API_PREFIX + '/refs/heads/master', function(){})
+
+		return this;
+	}
+}
+
+export default Github;
