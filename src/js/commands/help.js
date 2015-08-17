ed9f37b642fef0314018d183cae6414b2dcd0f9f
Jeff <jeffy9@gmail.com>
2015-08-17 15:38:15 -0400

    Update help

--- src/js/commands/help.js
+++ src/js/commands/help.js
@@ -2,16 +2,17 @@ export default function help() {
 
 var message = [
 
-	'Welcome to [[;#AB4642;]Jeff OS (JOS)]',
+	'Welcome to [[;#AB4642;]Jeff OS (JOS)]!',
+	'This is a UNIX - like simulated terminal.',
 	'\r',
-	'COMMANDS',
-	'    [[;#A1B56C;]help] Shows this menu',
-	'    [[;#A1B56C;]greeting] Shows the greeting message',
-	'    [[;#A1B56C;]motd] Shows a programmer quote from https://github.com/AntJanus/programmers-proverbs',
-	'    [[;#A1B56C;]run] Takes one parameter',
+	'SOME COMMANDS',
+	'    [[;#A1B56C;]help].....................Shows this menu',
+	'    [[;#A1B56C;]greeting].................Shows the greeting message',
+	'    [[;#A1B56C;]motd].....................Shows a programmer quote',
+/*	'    [[;#A1B56C;]run] Takes one parameter',
 	'    [[;#A1B56C;]fullscreen]',
 	'        [[;#F7CA88;]--enable] Go fullscreen ',
-	'        [[;#F7CA88;]--disable] Exit fullscreen ',
+	'        [[;#F7CA88;]--disable] Exit fullscreen ',*/
 ].join('\n');
 	this.echo('\n'+message+'\n')
 }
