642c3ac063e7f9cffec491dbfd801a72a3067ebf
Jeff <jeffy9@gmail.com>
2015-12-02 15:42:26 -0500

    Update proverb to work directly with service that has CORS enabled now

--- src/js/commands/motd.js
+++ src/js/commands/motd.js
@@ -4,28 +4,19 @@ var storage=$.localStorage;
 
 export default function motd(term=this) {
 
-	if(storage.isSet('proverbs')){
-		term.echo('"'+getRandomProverb()+'"\n');
+	if(storage.isSet('proverb')){
+		term.echo('"'+getProverb()+'"\n');
 	}
 
-	saveProverbs();
-
-
-	/*$.getJSON('http://anyorigin.com/dev/get?url=http%3A//proverbs-app.antjan.us/random&callback=?', function(data){
-		message = data.contents;
-		term
-		.resume()
-		.echo('"'+message+'"\n');
-	});*/
+	setProverb();
 }
 
-var getRandomProverb = function(){
-	var proverbs = storage.get('proverbs')
-	return proverbs[Math.floor((Math.random()*proverbs.length))]
+var getProverb = function(){
+	return storage.get('proverb');
 }
 
-var saveProverbs = function(){
-	$.getJSON('http://anyorigin.com/dev/get?url=http%3A//proverbs-app.antjan.us&callback=?', function(data){
-		storage.set('proverbs', data.contents)
+var setProverb = function(){
+	$.get( "http://proverbs-app.antjan.us/random", function(data) {
+		storage.set('proverb', data)
 	});
 }
