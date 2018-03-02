9c51727806ad896dd24fcba9fb3b98568fb83ed4
Jeff <jeffy9@gmail.com>
2018-03-02 14:54:24 -0500

    fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './public/dist'),
-    publicPath: '/public/dist/',
+    path: path.resolve(__dirname, './dist/dist'),
+    publicPath: '/dist/dist/',
     filename: 'build.js'
   },
   module: {
